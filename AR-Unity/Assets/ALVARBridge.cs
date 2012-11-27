838564040c2885afa0f32433497dceba7c7fe566
bara3r <bara3r+github@gmail.com>
2012-11-27 14:22:38 +0100

    Edit scene.

--- AR-Unity/Assets/ALVARBridge.cs
+++ AR-Unity/Assets/ALVARBridge.cs
@@ -7,18 +7,19 @@ using System.Runtime.InteropServices;
 // It imports the functions of the DLL in this special class.
 public class ALVARBridge {
 
-    [DllImport("ALVARBridge", EntryPoint = "alvar_init", CallingConvention = CallingConvention.Cdecl)]
+    //[DllImport("ALVARBridge", EntryPoint = "alvar_init", CallingConvention = CallingConvention.Cdecl)]
+    [DllImport("ALVARBridge")]
     public static extern void alvar_init(
 		int width, 
         int height);
 
-    [DllImport("ALVARBridge.dll")]
+    [DllImport("ALVARBridge")]
     public extern static void alvar_process(
         int[] imageData,
         double[] transMatrix);
 
     // This function seems buggy.
-    [DllImport("ALVARBridge.dll")]
+    [DllImport("ALVARBridge")]
     public extern static int alvar_number_of_detected_markers(int[] imageData);
 
     [DllImport("ALVARBridge", EntryPoint = "alvar_close", CallingConvention = CallingConvention.Cdecl)]
